d86c3da75de471483dc00a4fe17aa967d02ceda4
Ras-Al-Ghul98 <rasalghul240@gmail.com>
2023-03-16 12:15:47 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints  "A program that prints a line with puts function
+ * puzzle, followed by a newline
+ * Return: Always 0.
+ */
+int main(void)
+{
+	
+ 	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
