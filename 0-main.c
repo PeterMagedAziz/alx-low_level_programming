1bb9949f7f7d2ad4d0ef7bc5865669eb1c465cf2
Ras-Al-Ghul98 <rasalghul240@gmail.com>
2023-08-08 05:48:28 -0700

    second

--- 0-main.c
+++ 0-main.c
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-=======
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
->>>>>>> 511e4198f374eaf184122f565c6d50460e9ee6c6
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-<<<<<<< HEAD
 int main(int ac, char **av)
 {
     ssize_t n;
@@ -28,27 +20,5 @@ int main(int ac, char **av)
     printf("\n(printed chars: %li)\n", n);
     n = read_textfile(av[1], 1024);
     printf("\n(printed chars: %li)\n", n);
-=======
-int main(void)
-{
-    listint_t *head;
-    listint_t *new;
-    listint_t hello = {8, NULL};
-    size_t n;
-
-    head = &hello;
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->n = 9;
-    new->next = head;
-    head = new;
-    n = print_listint(head);
-    printf("-> %lu elements\n", n);
-    free(new);
->>>>>>> 511e4198f374eaf184122f565c6d50460e9ee6c6
     return (0);
 }
